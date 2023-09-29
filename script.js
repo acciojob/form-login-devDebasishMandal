794f5fb2394bcd3d93dc943e2666e8b48b2cb87c
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2023-09-29 23:24:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function getFormvalue() {
   //Write your code here
 let sub=document.getElementsByTagName("input");
-alert(sub[0].value+sub[1].value);
-
+window.alert(`${sub[0].value} ${sub[1].value}`);
 }
